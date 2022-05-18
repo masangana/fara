f2dd194ceb7a3651c8df24d9e32aab19abdce338
masangana <alexmasangana@gmail.com>
2022-05-18 12:23:08 +0300

    Add project file for new PR
    -- Create new repo
    -- Add Js files
    -- Add Css files
    -- Add Html files

---
+++ js/footer.js
@@ -0,0 +1,18 @@
+document.querySelector('#footer').innerHTML = `
+<img src="./ressources/icons/logo.png" alt="logo" class="footer-logo">
+<div class="footer-detail">
+    <p>Fara-Fara La compétition qu'il faut pour Vous</p>
+</div>`;
+
+document.querySelector('#sponsor').innerHTML = `
+<div class="sponsor-header">
+    <h2 class="section-title2">Nos Sponsors</h2>
+</div>
+<hr>
+<div class="sponsor-logos">
+    <img src="./ressources/sponsor/mozilla.png" alt="Mozilla" class="sponsor-logo">
+    <img src="./ressources/sponsor/Google.png" alt="Google" class="sponsor-logo">
+    <img src="./ressources/sponsor/navar.png" alt="Naver" class="sponsor-logo">
+    <img src="./ressources/sponsor/daumkakao.svg" alt="Daumkakao" class="sponsor-logo">
+    <img src="./ressources/sponsor/airbnb.png" alt="Airbnb" class="sponsor-logo">
+</div>`;
